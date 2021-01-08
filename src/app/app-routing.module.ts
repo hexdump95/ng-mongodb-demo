55a6762dc2962ebac040eeecae32bab4a9e7c9db
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2021-01-08 13:12:17 -0300

    Update AppRountingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthorComponent } from './authors/author/author.component';
+import { AuthorsComponent } from './authors/authors.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'authors', component: AuthorsComponent },
+  { path: 'authors/:id', component: AuthorComponent }
 ];
 
 @NgModule({
